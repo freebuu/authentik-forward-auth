c53f7efcd86512170915704f46c2d4870af3728a
Maksim <freebuu.dev@gmail.com>
2026-02-07 20:28:10 +0100

    init

---
+++ src/ForwardAuthServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FreeBuu\ForwardAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class ForwardAuthServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+    }
+}
